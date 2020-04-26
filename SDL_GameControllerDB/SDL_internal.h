e3e5a5e48b31d8a98838d6fe63eea2a791a67203
mogemimi <mogemimi@enginetrouble.net>
2020-04-27 00:40:44 +0900

    Add SDL_GameControllerDB header file

--- SDL_GameControllerDB/SDL_internal.h
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2013-2019 mogemimi. Distributed under the MIT license.
-
-// This dummy file prevents the following error when compiling SDL_GameControllerDB without SDL.
-// ```
-// SDL_GameControllerDB/data/SDL_gamecontrollerdb2.0.9.h:21:10: '../SDL_internal.h' file not found
-// ```
